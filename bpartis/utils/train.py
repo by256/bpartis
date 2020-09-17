1d48e70f16a6991265e32d493c4e771a20b4c9f8
by256 <by256@cam.ac.uk>
2020-09-17 11:01:00 +0100

    added load_pretrained

--- bpartis/utils/train.py
+++ bpartis/utils/train.py
@@ -35,8 +35,8 @@ def compute_decay_rate(start_lr, end_lr, epochs):
     return np.exp(np.log(end_lr/start_lr)/epochs)
 
 def train_test_split_emps(dataset, data_dir, im_size=(256, 256), device='cuda'):
-    train_dataset = dataset('{}/images/'.format(data_dir), '{}/segmaps/'.format(data_dir), im_size=im_size, device=device)
-    val_dataset = dataset('{}/images/'.format(data_dir), '{}/segmaps/'.format(data_dir), im_size=im_size, transform=False, device=device)
+    train_dataset = dataset('{}/processed-images/'.format(data_dir), '{}/segmaps/'.format(data_dir), im_size=im_size, device=device)
+    val_dataset = dataset('{}/processed-images/'.format(data_dir), '{}/segmaps/'.format(data_dir), im_size=im_size, transform=False, device=device)
 
     unique_dois = sorted(list(set([x.split('.png')[0].split(' (')[0].split('.gr')[0] for x in train_dataset.image_fns])))
 
@@ -52,3 +52,18 @@ def train_test_split_emps(dataset, data_dir, im_size=(256, 256), device='cuda'):
     train_dataset = split_train_val_dois(train_dataset, train_dois)
     val_dataset = split_train_val_dois(val_dataset, val_dois)
     return train_dataset, val_dataset
+
+def load_pretrained(model, path='./saved_models/', device='cuda'):
+    # load encoder
+    model.encoder.load_state_dict(torch.load('{}pretrained-encoder.pt'.format(path), map_location=device))
+    # load decoders
+    pretrained_dict = torch.load('{}pretrained-decoder.pt'.format(path), map_location=device)
+    for decoder in model.decoders:
+        model_dict = decoder.state_dict()
+        del model_dict['output_conv.weight']
+        del model_dict['output_conv.bias']
+        pretrained_dict_copy = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+        model_dict.update(pretrained_dict_copy) 
+        decoder.load_state_dict(pretrained_dict_copy, strict=False)
+    
+    return model
