adbc3b5aabd691c3e763ee166659e0e6a1f1dc8b
by256 <by256@cam.ac.uk>
2020-09-17 09:27:11 +0100

    added models

---
+++ bpartis/models.py
@@ -0,0 +1,220 @@
+"""
+## Models (much of this code was adapted from https://github.com/davyneven/SpatialEmbeddings
+## credit to Davy Neven.
+--------------------------------------------------
+## Author: Batuhan Yildirim
+## Email: by256@cam.ac.uk
+## Version: 1.0.0
+--------------------------------------------------
+## License: MIT
+## Copyright: Copyright Batuhan Yildirim, 2020, BPartIS
+-------------------------------------------------
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+
+class DownsamplerBlock (nn.Module):
+    def __init__(self, ninput, noutput):
+        super().__init__()
+
+        self.conv = nn.Conv2d(ninput, noutput-ninput,
+                              (3, 3), stride=2, padding=1, bias=True)
+        self.pool = nn.MaxPool2d(2, stride=2)
+        self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
+
+    def forward(self, input):
+        output = torch.cat([self.conv(input), self.pool(input)], 1)
+        output = self.bn(output)
+        return F.elu(output)
+
+
+class non_bottleneck_1d (nn.Module):
+    def __init__(self, chann, dropprob, dilated):
+        super().__init__()
+
+        self.conv3x1_1 = nn.Conv2d(
+            chann, chann, (3, 1), stride=1, padding=(1, 0), bias=True)
+
+        self.conv1x3_1 = nn.Conv2d(
+            chann, chann, (1, 3), stride=1, padding=(0, 1), bias=True)
+
+        self.bn1 = nn.BatchNorm2d(chann, eps=1e-03)
+
+        self.conv3x1_2 = nn.Conv2d(chann, chann, (3, 1), stride=1, padding=(
+            1*dilated, 0), bias=True, dilation=(dilated, 1))
+
+        self.conv1x3_2 = nn.Conv2d(chann, chann, (1, 3), stride=1, padding=(
+            0, 1*dilated), bias=True, dilation=(1, dilated))
+
+        self.bn2 = nn.BatchNorm2d(chann, eps=1e-03)
+
+        self.dropout = nn.Dropout2d(dropprob)
+
+    def forward(self, input):
+
+        output = self.conv3x1_1(input)
+        output = F.elu(output)
+        output = self.conv1x3_1(output)
+        output = self.bn1(output)
+        output = F.elu(output)
+
+        output = self.conv3x1_2(output)
+        output = F.elu(output)
+        output = self.conv1x3_2(output)
+        output = self.bn2(output)
+
+        if (self.dropout.p != 0):
+            output = self.dropout(output)
+
+        return F.elu(output+input)
+
+
+class Encoder(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.initial_block = DownsamplerBlock(3, 16)
+
+        self.layers = nn.ModuleList()
+
+        self.layers.append(DownsamplerBlock(16, 64))
+
+        for x in range(0, 5):  # 5 times
+            self.layers.append(non_bottleneck_1d(64, 0.03, 1))
+
+        self.layers.append(DownsamplerBlock(64, 128))
+
+        for x in range(0, 2):  # 2 times
+            self.layers.append(non_bottleneck_1d(128, 0.3, 2))
+            self.layers.append(non_bottleneck_1d(128, 0.3, 4))
+            self.layers.append(non_bottleneck_1d(128, 0.3, 8))
+            self.layers.append(non_bottleneck_1d(128, 0.3, 16))
+
+        # Only in encoder mode:
+        self.output_conv = nn.Conv2d(
+            128, num_classes, 1, stride=1, padding=0, bias=True)
+
+    def forward(self, input, predict=False):
+        output = self.initial_block(input)
+
+        for layer in self.layers:
+            output = layer(output)
+
+        if predict:
+            output = self.output_conv(output)
+
+        return output
+
+
+class UpsamplerBlock (nn.Module):
+    def __init__(self, ninput, noutput):
+        super().__init__()
+        self.conv = nn.Conv2d(ninput, noutput, 3, stride=1, padding=1, bias=True)
+        self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
+
+    def forward(self, input):
+        output = F.interpolate(input, scale_factor=2, mode='bilinear')
+        output = self.conv(output)
+        output = self.bn(output)
+        return F.elu(output)
+
+
+class Decoder (nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.layers = nn.ModuleList()
+
+        self.layers.append(UpsamplerBlock(128, 64))
+        self.layers.append(non_bottleneck_1d(64, 0, 1))
+        self.layers.append(non_bottleneck_1d(64, 0, 1))
+
+        self.layers.append(UpsamplerBlock(64, 16))
+        self.layers.append(non_bottleneck_1d(16, 0, 1))
+        self.layers.append(non_bottleneck_1d(16, 0, 1))
+
+        self.output_conv = nn.Conv2d(16, num_classes, 3, stride=1, padding=1, bias=True)
+
+    def forward(self, input):
+        output = input
+
+        for layer in self.layers:
+            output = layer(output)
+
+        output = F.interpolate(output, scale_factor=2, mode='bilinear')
+        output = self.output_conv(output)
+
+        return output
+
+
+# ERFNet
+
+class Net(nn.Module):
+    def __init__(self, num_classes, encoder=None):  # use encoder to pass pretrained encoder
+        super().__init__()
+
+        if (encoder == None):
+            self.encoder = Encoder(num_classes)
+        else:
+            self.encoder = encoder
+        self.decoder = Decoder(num_classes)
+
+    def forward(self, input, only_encode=False):
+        if only_encode:
+            return self.encoder.forward(input, predict=True)
+        else:
+            output = self.encoder(input)  # predict=False by default
+            return self.decoder.forward(output)
+
+
+class BranchedERFNet(nn.Module):
+    def __init__(self, num_classes, encoder=None):
+        super().__init__()
+
+        print('Creating branched erfnet with {} classes'.format(num_classes))
+
+        if (encoder is None):
+            self.encoder = Encoder(sum(num_classes))
+        else:
+            self.encoder = encoder
+
+        self.decoders = nn.ModuleList()
+        for n in num_classes:
+            self.decoders.append(Decoder(n))
+
+    def init_output(self, n_sigma=1):
+        with torch.no_grad():
+            output_conv = self.decoders[0].output_conv
+            print('initialize last layer with size: ',
+                  output_conv.weight.size())
+
+            output_conv.weight[:, 0:2, :, :].fill_(0)
+            output_conv.bias[0:2].fill_(0)
+
+            output_conv.weight[:, 2:2+n_sigma, :, :].fill_(0)
+            output_conv.bias[2:2+n_sigma].fill_(1)
+
+    def forward(self, input, only_encode=False):
+        if only_encode:
+            return self.encoder.forward(input, predict=True)
+        else:
+            output = self.encoder(input)
+
+        return torch.cat([decoder.forward(output) for decoder in self.decoders], 1)
+
+
+class UnsupNet(nn.Module):
+
+    def __init__(self):
+        super(UnsupNet, self).__init__()
+        self.encoder = Encoder(5)
+        self.decoder = Decoder(3)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        x_prime = self.decoder(z)
+        return z, x_prime
