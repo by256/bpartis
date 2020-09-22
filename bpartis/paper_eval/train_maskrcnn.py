9e89a4e88395419e6476b51916ffb5c76023ffa8
by256 <by256@cam.ac.uk>
2020-09-22 19:47:20 +0100

    add train_maskrcnn

--- bpartis/paper_eval/train_maskrcnn.py
+++ bpartis/paper_eval/train_maskrcnn.py
@@ -10,17 +10,105 @@
 -----
 """
 
+import os
+import sys
 import time
 import pickle
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+py_dll_path = os.path.join(sys.exec_prefix, 'Library', 'bin')
+os.environ['PATH'] += py_dll_path
 import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ExponentialLR
+from torchvision.models.detection.mask_rcnn import MaskRCNN
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
-from ..data import EMPSMaskRCNN
-from ..utils.train import train_test_split_emps, compute_decay_rate
+sys.path.append('..')
+from data import EMPSMaskRCNN
+from utils.eval import metrics
+from utils.train import train_test_split_emps, compute_decay_rate
 
 
+parser = argparse.ArgumentParser(description='Train Mask R-CNN model on EMPS dataset.')
+parser.add_argument('--data-dir', metavar='data_dir', type=str, help='Directory which contains the data.')
+parser.add_argument('--device', metavar='device', type=str, default='cuda', help='device to train on (cuda or cpu)')
+parser.add_argument('--im-size', metavar='im_size', type=tuple, default=(256, 256), help='Image size to load for training.')
+parser.add_argument('--save-dir', metavar='save_dir', type=str, default='./saved_models/', help='directory to save and load weights from.')
+parser.add_argument('--batch-size', metavar='batch_size', type=int, default=1, help='Batch size for training.')
+parser.add_argument('--lr', metavar='lr', type=float, default=3e-4, help='Learning rate.')
+parser.add_argument('--end-lr', metavar='end_lr', type=float, default=None, help='Learning rate to decay to.')
+parser.add_argument('--epochs', metavar='epochs', type=int, default=300, help='No. of epochs to train.')
+namespace = parser.parse_args()
+
+train_dataset, val_dataset = train_test_split_emps(EMPSMaskRCNN, 
+                                                   namespace.data_dir, 
+                                                   im_size=namespace.im_size, 
+                                                   device=namespace.device)
+
+# train_dataset.image_fns = train_dataset.image_fns[:10]
+# val_dataset.image_fns = val_dataset.image_fns[:10]
+
+
+print('Train: {}    Val: {}'.format(len(train_dataset), len(val_dataset)))
+
+collate_fn = lambda x: tuple(zip(*x))
+train_loader = DataLoader(train_dataset, batch_size=namespace.batch_size, shuffle=True, collate_fn=collate_fn)
+val_loader = DataLoader(val_dataset, batch_size=namespace.batch_size, collate_fn=collate_fn)
+
+backbone = resnet_fpn_backbone('resnet101', pretrained=True)
+backbone.requires_grad = False
+model = MaskRCNN(backbone=backbone, num_classes=2).to(namespace.device)
+
+optimizer = Adam([p for p in model.parameters() if p.requires_grad], lr=namespace.lr)
+if namespace.end_lr is not None:
+    decay = compute_decay_rate(start_lr=namespace.lr, end_lr=namespace.end_lr, epochs=int(namespace.epochs*0.75))
+    lr_scheduler = ExponentialLR(optimizer=optimizer, gamma=decay)
+
+losses = {'train': [], 'val-iou': [], 'val-ap': []}
+
+for epoch in range(namespace.epochs):
+
+    epoch_train_losses = []
+    start = time.time()
+    model.train()
+
+    for (images, targets) in train_loader:
+        images = list(image.to(namespace.device) for image in images)
+        targets = [{k: v.to(namespace.device) for k, v in t.items()} for t in targets]
+        loss_dict = model(images, targets)
+        loss = loss_dict['loss_classifier'] + loss_dict['loss_box_reg'] + loss_dict['loss_mask']
+        epoch_train_losses.append(loss.item())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    losses['train'].append(np.mean(epoch_train_losses))
+
+    epoch_val_ious = []
+    epoch_val_aps = []
+    model.eval()
+    for (images, targets) in val_loader:
+        images = list(image.to(namespace.device) for image in images)
+        targets = [{k: v.to(namespace.device) for k, v in t.items()} for t in targets]
+        outputs = model(images)
+        for i, output in enumerate(outputs):
+            pred_masks = output['masks'].squeeze()
+            gt_masks = targets[i]['masks']
+            iou, ap = metrics(pred_masks, gt_masks)
+            print('iou', iou, 'ap', ap)
+    
+    if (namespace.end_lr is not None) & (epoch+1 <= int(namespace.epochs*0.75)):
+        lr_scheduler.step()
+
+    save_model = (np.mean(epoch_val_ious) < np.min(losses['val-iou']) if epoch > 0 else False)
+    losses['val-iou'].append(np.mean(epoch_val_ious))
+    losses['val-ap'].append(np.mean(epoch_val_aps))
+
+    print('{}/{}    Train: {:.5f}    Val IOU: {:.5f}    Val AP: {:.5f}    lr: {:.9f}    T: {:.2f} s'.format(epoch+1, namespace.epochs, losses['train'][-1], losses['val-iou'][-1], losses['val-ap'][-1], optimizer.param_groups[-1]['lr'], time.time()-start))
+
+    if save_model:
+        torch.save(model.state_dict(), '{}mask-rcnn.pt'.format(namespace.save_dir))
