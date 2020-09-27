c19ed8343243a72e211a584d8dd6d8a370adfff3
by256 <by256@cam.ac.uk>
2020-09-27 16:19:03 +0100

    final fix ap metrics

--- bpartis/utils/eval.py
+++ bpartis/utils/eval.py
@@ -19,32 +19,70 @@ def compute_iou(pred_mask, gt_mask):
     Computes IoU between predicted instance mask and 
     ground-truth instance mask
     """
-    # intersection = pred_mask & gt_mask
-    # union = pred_mask | gt_mask
+
     pred_mask = pred_mask.byte()
     gt_mask = gt_mask.byte()
     intersection = torch.bitwise_and(pred_mask, gt_mask).sum().float()
     union = torch.bitwise_or(pred_mask, gt_mask).sum().float()
     return intersection / union
 
-def compute_matches(pred, gt, t=0.5):
-    matches = []
+def compute_tp_fp_fn(pred, gt, t=0.5):
     ious = []
-    for pred_idx, pred_mask in enumerate(pred):
-        for gt_idx, gt_mask in enumerate(gt):
-            iou = compute_iou(pred_mask, gt_mask)
-            if iou > t:
-                match = (pred_idx, gt_idx)
-                matches.append(match)
-                ious.append(iou)
-                break
-    if len(ious) == 0:
-        mean_iou = 0.0
-    else:
-        mean_iou = np.mean(ious)
-    return matches, mean_iou
+    tp = 0
+    fp = 0
+    fn = 0
+                  # gt_idx, pred_idx, match, iou
+    match_table = []
 
-def metrics(pred, gt, t=0.5, eps=1e-12):
+    for gt_idx, gt_mask in enumerate(gt):
+        for pred_idx, pred_mask in enumerate(pred):
+            iou = compute_iou(gt_mask, pred_mask)
+            match = 1 if iou > t else 0
+            row = [gt_idx, pred_idx, match, iou]
+            match_table.append(row)
+    
+    match_table = np.array(match_table)
+
+    # get tp and fn
+    for gt_idx in range(len(gt)):
+        current_idx_mt = match_table[match_table[:, 0] == gt_idx]
+        # print(current_idx_mt, '\n')
+        n_matches = np.sum(current_idx_mt[:, 2], dtype=int)
+        if n_matches > 0:
+            tp += 1
+        else:
+            fn += 1
+        
+        # penalize duplicates
+        n_iou_gt_thresh = np.sum((current_idx_mt[:, 3] > t).astype(int))
+        if n_iou_gt_thresh > 1:
+            fp += n_matches - 1
+
+    # get fp
+    for pred_idx in range(len(pred)):
+        current_idx_mt = match_table[match_table[:, 1] == pred_idx]
+        n_matches = np.sum(current_idx_mt[:, 2], dtype=int)
+        n_iou_gt_zero = np.sum((current_idx_mt[:, 3] > 0.0).astype(int))
+
+        if n_matches > 0:
+            max_iou = np.max(current_idx_mt[:, 3])
+            ious.append(max_iou)
+
+        if (n_matches == 0):
+            fp += 1
+            ious.append(0.0)
+        
+    mean_iou = np.mean(ious) if len(ious) > 0  else 0.0
+
+    return mean_iou, tp, fp, fn
+
+
+def metrics(pred, gt, eps=1e-12):
+
+    if isinstance(pred, np.ndarray):
+        pred = torch.Tensor(pred)
+    if isinstance(gt, np.ndarray):
+        gt = torch.Tensor(gt)
 
     pred = pred.detach()
 
@@ -53,61 +91,107 @@ def metrics(pred, gt, t=0.5, eps=1e-12):
         pred = torch.stack([(pred == i).byte() for i in torch.unique(pred) if i != 0], dim=0)
         gt = torch.stack([(gt == i).byte() for i in torch.unique(gt) if i != 0], dim=0)
 
-    matches, mean_iou = compute_matches(pred, gt)
-    tp = len(matches)
-    fp = np.maximum(len(pred) - tp, 0)
-    fn = np.maximum(len(gt) - tp, 0)
-
-    precision = tp / (tp + fp + eps)
-
-    return mean_iou, precision
-
-def average_precision_range(pred, gt, eps=1e-12):
-
-    if len(gt.shape) == 2:
-        # turn instance maps into N binary instance maps each
-        pred = torch.stack([(pred == i).byte() for i in torch.unique(pred) if i != 0], dim=0)
-        gt = torch.stack([(gt == i).byte() for i in torch.unique(gt) if i != 0], dim=0)
-
-    aps = []
-    thresholds = np.linspace(0.5, 0.95, 10)
+    ap = []
+    ar = []
+    ious = []
+    
+    # thresholds = np.linspace(0.5, 0.95, 10)
+    thresholds = np.linspace(0.0, 0.95, 20)
     for t in thresholds:
-        matches, _ = compute_matches(pred, gt, t=t)
-        tp = len(matches)
-        fp = np.maximum(len(pred) - tp, 0.0)
-        fn = np.maximum(len(gt) - tp, 0.0)
+        t = np.round(t, decimals=3)  # for float precision errors
+        mean_iou, tp, fp, fn = compute_tp_fp_fn(pred, gt, t=t)
         precision = tp / (tp + fp + eps)
-        aps.append(precision)
-    return np.mean(aps)
+        recall = tp / (tp + fn + eps)
+        # print(f't {t :.2f}  iou {mean_iou :.3f}  precision {precision :.3f}  recall {recall :.5f}  tp {tp}  fp {fp}  fn {fn}')
 
+        ap.append(precision)
+        ar.append(recall)
+        if t == 0.5:
+            ap_50 = precision
+            ious.append(mean_iou)
+        elif t == 0.75:
+            ap_75 = precision
+    
+    ap = np.array(ap)[::-1]
+    ap = make_monotonic(ap)
+    ar = np.array(ar)[::-1]
+
+    if np.all(np.array(ar, dtype=np.float16) == 1.0) and np.all(np.array(ap, dtype=np.float16) == 1.0):
+        ap = 1.0
+    elif np.all(np.array(ar, dtype=np.float16) == np.mean(ar)) and np.all(np.array(ap, dtype=np.float16) == np.mean(ap)):
+        ap = np.mean(ap)
+    else:
+        ap = np.sum((ar[1:] - ar[:-1]) * (ap[1:] + ap[:-1]) / 2)
+    mean_iou = np.mean(ious)
+
+    return mean_iou, ap, ap_50, ap_75
+
+
+def make_monotonic(x):
+    for i in range(len(x)-2, -1, -1):
+        x[i] = max(x[i], x[i+1])
+    return x
 
 
 # import os
 # import sys
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-# from data import EMPSMaskRCNN
-# from utils.train import train_test_split_emps
+# import matplotlib.pyplot as plt
+# from data import EMPSMaskRCNN, EMPSDataset
+# from models import BranchedERFNet
+# from utils.train import train_test_split_emps, load_pretrained
+# from cluster import Cluster
 
 # device='cpu'
-# train_dataset, val_dataset = train_test_split_emps(EMPSMaskRCNN, 
+
+# train_dataset, val_dataset = train_test_split_emps(EMPSDataset, 
 #                                                    '/home/by256/Documents/Projects/particle-seg-dataset/elsevier/', 
 #                                                    im_size=(512, 512), 
 #                                                    device=device)
 
-# # model = BranchedERFNet(num_classes=[4, 1]).to(device)
-# # model = load_pretrained(model, path='/home/by256/Documents/Projects/bpartis/bpartis/saved_models/emps-model.pt', device=device)
+# model = BranchedERFNet(num_classes=[4, 1]).to(device)
+# model.load_state_dict(torch.load('/home/by256/Documents/Projects/bpartis/bpartis/saved_models/emps-model.pt', map_location=device))
+# model.eval()
 
-# # image, instance, _ = val_dataset[0]
+# cluster = Cluster(n_sigma=2, device=device)
 
-# image, targets = val_dataset[0]
-# masks = targets['masks']
+# ious = []
+# aps = []
+# ap_50s = []
+# ap_75s = []
 
-# image, targets = val_dataset[4]
-# masks_2 = targets['masks']
+# for i in range(len(val_dataset)):
+#     # if i < 46: # 7 doesn't work
+#     #     continue
+#     image, instance, _ = val_dataset[i]  # 37, 38
+#     image = image.unsqueeze(0)
 
-# # print(targets)
-# # print(masks.shape)
+#     output = model(image).squeeze()
 
-# iou, ap = metrics(masks, masks_2)
+#     pred, _ = cluster.cluster(output)
+#     pred = pred.to(device)
 
-# print(iou, ap)
+#     iou, ap, ap_50, ap_75 = metrics(pred, instance)
+#     print('{}/{}    IoU: {:.5f}   AP: {:.5f}   AP_50: {:.5f}   AP_75: {:.5f}'.format(i+1, len(val_dataset), iou, ap, ap_50, ap_75))
+
+#     ious.append(iou)
+#     aps.append(ap)
+#     ap_50s.append(ap_50)
+#     ap_75s.append(ap_75)
+#     # print('{}/{}'.format(i+1, len(val_dataset)), flush=True, end='\r')
+
+#     # fig, axes = plt.subplots(1, 3)
+#     # for ax in axes:
+#     #     ax.axis('off')
+#     # axes[0].imshow(image.squeeze().permute(1, 2, 0).cpu())
+#     # axes[1].matshow(pred.cpu(), cmap='tab20')
+#     # axes[1].set_title('Pred') 
+#     # axes[2].matshow(instance.cpu(), cmap='tab20')
+#     # axes[2].set_title('GT')
+#     # plt.show()
+#     # break
+
+# print('IoU: {:.5f}   AP: {:.5f}   AP_50: {:.5f}   AP_75: {:.5f}'.format(np.mean(ious), 
+#                                                                         np.mean(aps), 
+#                                                                         np.mean(ap_50s), 
+#                                                                         np.mean(ap_75s)))
