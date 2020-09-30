1ac34fbb40016402072a23672a6370f69f7d01a5
by256 <by256@cam.ac.uk>
2020-09-30 11:07:06 +0100

    mask

--- bpartis/paper_eval/train_maskrcnn.py
+++ bpartis/paper_eval/train_maskrcnn.py
@@ -26,8 +26,6 @@ from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 sys.path.append('../../')
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-from bpartis.data import EMPSMaskRCNN
-from bpartis.utils.train import train_test_split_emps, compute_decay_rate
 
 
 parser = argparse.ArgumentParser(description='Train Mask R-CNN model on EMPS dataset.')
@@ -40,6 +38,110 @@ parser.add_argument('--lr', metavar='lr', type=float, default=3e-4, help='Learni
 parser.add_argument('--end-lr', metavar='end_lr', type=float, default=None, help='Learning rate to decay to.')
 parser.add_argument('--epochs', metavar='epochs', type=int, default=300, help='No. of epochs to train.')
 namespace = parser.parse_args()
+
+def split_train_val_dois(dataset, dois):
+    new_fns = []
+    for fn in dataset.image_fns:
+        keep_fn = False
+        for doi in dois:
+            if doi in fn:
+                keep_fn = True
+        if keep_fn:
+            new_fns.append(fn)
+    dataset.image_fns = new_fns
+    return dataset
+
+def freeze_batchnorm_layers(model):
+    for module in model.modules():
+        if isinstance(module, torch.nn.modules.BatchNorm2d):
+            module.eval()
+
+def compute_decay_rate(start_lr, end_lr, epochs):
+    return np.exp(np.log(end_lr/start_lr)/epochs)
+
+def train_test_split_emps(dataset, data_dir, im_size=(512, 512), device='cuda'):
+    train_dataset = dataset('{}/processed-images/'.format(data_dir), '{}/segmaps/'.format(data_dir), im_size=im_size, device=device)
+    val_dataset = dataset('{}/processed-images/'.format(data_dir), '{}/segmaps/'.format(data_dir), im_size=im_size, transform=False, device=device)
+
+    unique_dois = sorted(list(set([x.split('.png')[0].split(' (')[0].split('.gr')[0] for x in train_dataset.image_fns])))
+
+    np.random.seed(15)
+    indices = np.arange(len(unique_dois))
+    np.random.shuffle(indices)
+
+    # train_indices = indices[:int(0.88*len(indices))]
+    # val_indices = indices[int(0.88*len(indices)):]
+    train_indices = indices[:int(0.77*len(indices))]
+    val_indices = indices[int(0.77*len(indices)):]
+    train_dois = [unique_dois[i] for i in train_indices]
+    val_dois = [unique_dois[i] for i in val_indices]
+
+    train_dataset = split_train_val_dois(train_dataset, train_dois)
+    val_dataset = split_train_val_dois(val_dataset, val_dois)
+    return train_dataset, val_dataset
+
+class EMPSMaskRCNN(Dataset):
+    
+    def __init__(self, image_dir, mask_dir, im_size=(256, 256), device='cuda', transform=False):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.im_size = im_size
+        self.device = device
+
+        self.image_fns = os.listdir(image_dir)
+        self.image_fns = [x for x in self.image_fns if x.endswith('.png')]
+
+        np.random.seed(9)
+        shuffle_idx = np.arange(len(self.image_fns)).astype(int)
+        np.random.shuffle(shuffle_idx)
+
+        self.image_fns = list(np.array(self.image_fns)[shuffle_idx])
+
+    def __len__(self):
+        return len(self.image_fns)
+
+    def __getitem__(self, idx):
+        image = np.array(Image.open(self.image_dir + self.image_fns[idx]).resize(self.im_size, resample=Image.BICUBIC))
+        image = torch.Tensor(image).permute(2, 0, 1)
+        mask = np.array(Image.open(self.mask_dir + self.image_fns[idx]).resize(self.im_size, resample=Image.NEAREST))
+        obj_ids = np.unique(mask)[1:]
+        
+        # split the color-encoded mask into a set of binary masks
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.Tensor(boxes).float()
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.Tensor(masks).byte()
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        return image, target
+
+
 
 train_dataset, val_dataset = train_test_split_emps(EMPSMaskRCNN, 
                                                    namespace.data_dir, 
