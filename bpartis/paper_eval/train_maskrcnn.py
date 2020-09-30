21c96b2f6ed7379d950e13ae2fbd307031759ba9
by256 <by256@cam.ac.uk>
2020-09-30 10:41:10 +0100

    working

--- bpartis/paper_eval/train_maskrcnn.py
+++ bpartis/paper_eval/train_maskrcnn.py
@@ -24,10 +24,9 @@ from torch.optim.lr_scheduler import ExponentialLR
 from torchvision.models.detection.mask_rcnn import MaskRCNN
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-from data import EMPSMaskRCNN
-from utils.eval import metrics
-from utils.train import train_test_split_emps, compute_decay_rate
+sys.path.append('../../')
+from bpartis.data import EMPSMaskRCNN
+from bpartis.utils.train import train_test_split_emps, compute_decay_rate
 
 
 parser = argparse.ArgumentParser(description='Train Mask R-CNN model on EMPS dataset.')
@@ -65,56 +64,70 @@ if namespace.end_lr is not None:
     decay = compute_decay_rate(start_lr=namespace.lr, end_lr=namespace.end_lr, epochs=int(namespace.epochs*0.75))
     lr_scheduler = ExponentialLR(optimizer=optimizer, gamma=decay)
 
-losses = {'train': [], 'val-iou': [], 'val-ap': [], 'val-ap-50': []}
+from engine import train_one_epoch, evaluate
+import utils
 
 for epoch in range(namespace.epochs):
+    # train for one epoch, printing every 10 iterations
+    train_one_epoch(model, optimizer, train_loader, namespace.device, epoch, print_freq=10)
+    # update the learning rate
+    if (namespace.end_lr is not None) & (epoch+1 <= int(namespace.epochs*0.75)):
+        lr_scheduler.step()
+    # evaluate on the test dataset
+    evaluate(model, val_loader, device=namespace.device)
 
-    epoch_train_losses = []
-    start = time.time()
-    model.train()
+    torch.save(model.state_dict(), '{}mask-rcnn.pt'.format(namespace.save_dir))
 
-    for (images, targets) in train_loader:
-        images = list(image.to(namespace.device) for image in images)
-        targets = [{k: v.to(namespace.device) for k, v in t.items()} for t in targets]
-        loss_dict = model(images, targets)
-        loss = loss_dict['loss_classifier'] + loss_dict['loss_box_reg'] + loss_dict['loss_mask']
-        epoch_train_losses.append(loss.item())
+# losses = {'train': [], 'val-iou': [], 'val-ap': [], 'val-ap-50': []}
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+# for epoch in range(namespace.epochs):
 
-    losses['train'].append(np.mean(epoch_train_losses))
+#     epoch_train_losses = []
+#     start = time.time()
+#     model.train()
 
-    epoch_val_ious = []
-    epoch_val_aps = []
-    epoch_val_ap_50s = []
-    model.eval()
+#     for (images, targets) in train_loader:
+#         images = list(image.to(namespace.device) for image in images)
+#         targets = [{k: v.to(namespace.device) for k, v in t.items()} for t in targets]
+#         loss_dict = model(images, targets)
+#         loss = loss_dict['loss_classifier'] + loss_dict['loss_box_reg'] + loss_dict['loss_mask']
+#         epoch_train_losses.append(loss.item())
 
-    if epoch > 10:
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
 
-        for (images, targets) in val_loader:
-            images = list(image.to(namespace.device) for image in images)
-            targets = [{k: v.to(namespace.device) for k, v in t.items()} for t in targets]
-            outputs = model(images)
-            for i, output in enumerate(outputs):
-                pred_masks = output['masks'].detach()
-                gt_masks = targets[i]['masks'].detach()
-                # print('pred_masks', pred_masks.shape, 'gt_masks', gt_masks.shape)
-                iou, ap, ap_50, ap_75 = metrics(pred_masks, gt_masks)
-                epoch_val_ious.append(iou)
-                epoch_val_aps.append(ap)
-                epoch_val_ap_50s.append(ap_50)
+#     losses['train'].append(np.mean(epoch_train_losses))
+
+#     epoch_val_ious = []
+#     epoch_val_aps = []
+#     epoch_val_ap_50s = []
+#     model.eval()
+
+#     if epoch > 10:
+
+#         for (images, targets) in val_loader:
+#             images = list(image.to(namespace.device) for image in images)
+#             targets = [{k: v.to(namespace.device) for k, v in t.items()} for t in targets]
+#             outputs = model(images)
+#             for i, output in enumerate(outputs):
+#                 pred_masks = output['masks'].detach()
+#                 gt_masks = targets[i]['masks'].detach()
+#                 # print('pred_masks', pred_masks.shape, 'gt_masks', gt_masks.shape)
+#                 iou, ap, ap_50, ap_75 = metrics(pred_masks, gt_masks)
+#                 epoch_val_ious.append(iou)
+#                 epoch_val_aps.append(ap)
+#                 epoch_val_ap_50s.append(ap_50)
         
-        if (namespace.end_lr is not None) & (epoch+1 <= int(namespace.epochs*0.75)):
-            lr_scheduler.step()
+#         if (namespace.end_lr is not None) & (epoch+1 <= int(namespace.epochs*0.75)):
+#             lr_scheduler.step()
 
-    save_model = (np.mean(epoch_val_ious) < np.min(losses['val-iou']) if epoch > 0 else False)
-    losses['val-iou'].append(np.mean(epoch_val_ious))
-    losses['val-ap'].append(np.mean(epoch_val_aps))
-    losses['val-ap-50'].append(np.mean(epoch_val_ap_50s))
+#     save_model = (np.mean(epoch_val_ious) < np.min(losses['val-iou']) if epoch > 0 else False)
+#     losses['val-iou'].append(np.mean(epoch_val_ious))
+#     losses['val-ap'].append(np.mean(epoch_val_aps))
+#     losses['val-ap-50'].append(np.mean(epoch_val_ap_50s))
 
-    print('{}/{}    Train: {:.5f}    Val IOU: {:.5f}    Val AP: {:.5f}    Val AP_50: {:.5f}    lr: {:.9f}    T: {:.2f} s'.format(epoch+1, namespace.epochs, losses['train'][-1], losses['val-iou'][-1], losses['val-ap'][-1], losses['val-ap-50'][-1], optimizer.param_groups[-1]['lr'], time.time()-start))
+#     print('{}/{}    Train: {:.5f}    Val IOU: {:.5f}    Val AP: {:.5f}    Val AP_50: {:.5f}    lr: {:.9f}    T: {:.2f} s'.format(epoch+1, namespace.epochs, losses['train'][-1], losses['val-iou'][-1], losses['val-ap'][-1], losses['val-ap-50'][-1], optimizer.param_groups[-1]['lr'], time.time()-start))
 
-    if save_model:
-        torch.save(model.state_dict(), '{}mask-rcnn.pt'.format(namespace.save_dir))
+#     if save_model:
+#         torch.save(model.state_dict(), '{}mask-rcnn.pt'.format(namespace.save_dir))
