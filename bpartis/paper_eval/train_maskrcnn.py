a053ac2efee1ef78f143f0e374699109381efb5f
by256 <by256@cam.ac.uk>
2020-09-30 08:43:15 +0100

    maskrcnn ready to test

--- bpartis/paper_eval/train_maskrcnn.py
+++ bpartis/paper_eval/train_maskrcnn.py
@@ -35,7 +35,7 @@ parser.add_argument('--data-dir', metavar='data_dir', type=str, help='Directory 
 parser.add_argument('--device', metavar='device', type=str, default='cuda', help='device to train on (cuda or cpu)')
 parser.add_argument('--im-size', metavar='im_size', type=tuple, default=(512, 512), help='Image size to load for training.')
 parser.add_argument('--save-dir', metavar='save_dir', type=str, default='./saved_models/', help='directory to save and load weights from.')
-parser.add_argument('--batch-size', metavar='batch_size', type=int, default=1, help='Batch size for training.')
+parser.add_argument('--batch-size', metavar='batch_size', type=int, default=5, help='Batch size for training.')
 parser.add_argument('--lr', metavar='lr', type=float, default=3e-4, help='Learning rate.')
 parser.add_argument('--end-lr', metavar='end_lr', type=float, default=None, help='Learning rate to decay to.')
 parser.add_argument('--epochs', metavar='epochs', type=int, default=300, help='No. of epochs to train.')
@@ -46,8 +46,8 @@ train_dataset, val_dataset = train_test_split_emps(EMPSMaskRCNN,
                                                    im_size=namespace.im_size, 
                                                    device=namespace.device)
 
-# train_dataset.image_fns = train_dataset.image_fns[:10]
-# val_dataset.image_fns = val_dataset.image_fns[:10]
+# train_dataset.image_fns = train_dataset.image_fns[:20]
+# val_dataset.image_fns = val_dataset.image_fns[:4]
 
 
 print('Train: {}    Val: {}'.format(len(train_dataset), len(val_dataset)))
@@ -65,7 +65,7 @@ if namespace.end_lr is not None:
     decay = compute_decay_rate(start_lr=namespace.lr, end_lr=namespace.end_lr, epochs=int(namespace.epochs*0.75))
     lr_scheduler = ExponentialLR(optimizer=optimizer, gamma=decay)
 
-losses = {'train': [], 'val-iou': [], 'val-ap': []}
+losses = {'train': [], 'val-iou': [], 'val-ap': [], 'val-ap-50': []}
 
 for epoch in range(namespace.epochs):
 
@@ -88,17 +88,20 @@ for epoch in range(namespace.epochs):
 
     epoch_val_ious = []
     epoch_val_aps = []
+    epoch_val_ap_50s = []
     model.eval()
     for (images, targets) in val_loader:
         images = list(image.to(namespace.device) for image in images)
         targets = [{k: v.to(namespace.device) for k, v in t.items()} for t in targets]
         outputs = model(images)
         for i, output in enumerate(outputs):
-            pred_masks = output['masks'].squeeze()
+            pred_masks = output['masks']#.squeeze()
             gt_masks = targets[i]['masks']
-            iou, ap = metrics(pred_masks, gt_masks)
+            # print('pred_masks', pred_masks.shape, 'gt_masks', gt_masks.shape)
+            iou, ap, ap_50, ap_75 = metrics(pred_masks, gt_masks)
             epoch_val_ious.append(iou)
             epoch_val_aps.append(ap)
+            epoch_val_ap_50s.append(ap_50)
     
     if (namespace.end_lr is not None) & (epoch+1 <= int(namespace.epochs*0.75)):
         lr_scheduler.step()
@@ -106,8 +109,9 @@ for epoch in range(namespace.epochs):
     save_model = (np.mean(epoch_val_ious) < np.min(losses['val-iou']) if epoch > 0 else False)
     losses['val-iou'].append(np.mean(epoch_val_ious))
     losses['val-ap'].append(np.mean(epoch_val_aps))
+    losses['val-ap-50'].append(np.mean(epoch_val_ap_50s))
 
-    print('{}/{}    Train: {:.5f}    Val IOU: {:.5f}    Val AP: {:.5f}    lr: {:.9f}    T: {:.2f} s'.format(epoch+1, namespace.epochs, losses['train'][-1], losses['val-iou'][-1], losses['val-ap'][-1], optimizer.param_groups[-1]['lr'], time.time()-start))
+    print('{}/{}    Train: {:.5f}    Val IOU: {:.5f}    Val AP: {:.5f}    Val AP_50: {:.5f}    lr: {:.9f}    T: {:.2f} s'.format(epoch+1, namespace.epochs, losses['train'][-1], losses['val-iou'][-1], losses['val-ap'][-1], losses['val-ap-50'][-1], optimizer.param_groups[-1]['lr'], time.time()-start))
 
     if save_model:
         torch.save(model.state_dict(), '{}mask-rcnn.pt'.format(namespace.save_dir))
